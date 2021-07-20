f8092cbc89606460c0744b246651e76fad522db2
阎继先 <yjx3097890@sina.com>
2021-07-21 00:30:46 +0800

    Update SearchViewController.swift

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -84,7 +84,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        // test 123
+        // test 123321
         
     }
     
